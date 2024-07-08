3070aee4e669b673fadae16cd3b46e0550e85f28
karan chugh <karanchugh@karan-ka-MacBook-Pro.local>
2024-07-08 17:45:50 +0530

    fix: id generation in event webhook table

--- src/migrations/add-event-webhooks1627882164131.ts
+++ src/migrations/add-event-webhooks1627882164131.ts
@@ -4,12 +4,12 @@ export class addEventWebhooks1627882164131 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
       CREATE TABLE event_webhooks (
-        id VARCHAR(255) PRIMARY KEY,
+        "id" character varying NOT NULL DEFAULT gen_random_uuid(),
         webhook_url VARCHAR(255) NOT NULL,
         event_types JSON NOT NULL DEFAULT '[]',
         created_at TIMESTAMP DEFAULT NOW(),
         updated_at TIMESTAMP DEFAULT NOW(),
-        deleted_at TIMESTAMP NULL
+        constraint "event-webhooks-pkey" primary key ("id")
       );
     `);
   }
--- src/models/event-webhooks.ts
+++ src/models/event-webhooks.ts
@@ -1,14 +1,31 @@
-import { BaseEntity, BeforeInsert, Column, Entity } from "typeorm";
+import {
+  BaseEntity,
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
 import { SoftDeletableEntity } from "@medusajs/medusa";
 import { generateEntityId } from "@medusajs/medusa/dist/utils";
 
 @Entity({ name: "event_webhooks" })
-export default class EventWebhooks extends SoftDeletableEntity {
+export default class EventWebhooks {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
   @Column({ type: "varchar", nullable: false })
   webhook_url: string;
 
   @Column({ type: "json", nullable: false, default: [] })
   event_types: string[];
+
+  @CreateDateColumn({ type: "timestamptz" })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: "timestamptz" })
+  updated_at: Date;
 
   @BeforeInsert()
   private beforeInsert(): void {
